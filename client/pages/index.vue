fad95abf689944da58a81d12670589788e98cd2c
daniel_sim <daniel_sim@data.gov.sg>
2017-08-19 11:00:25 +0800

    client part

---
+++ client/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="panel-container">
+    <section class="panel">
+      <input type="text" v-model="searchString" />
+
+      <ul>
+        <li v-for="vehicle in vehicles">{{vehicle.registrationno}}</li>
+      </ul>
+
+    </section>
+    <GmapMap :center="{lat: 1.38, lng: 103.8}" :zoom="11" class="panel">
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+.panel-container {
+  position: fixed;
+  top: 0; left: 0; bottom: 0; right: 0;
+  display: flex;
+
+  .panel {
+    flex: 1 1 50%;
+  }
+}
+</style>
